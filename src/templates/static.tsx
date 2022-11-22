34b4ea2910f720364b3191cd24c82d79ba7fe83c
Maria Chiara Delli Carri <mc.dellicarri@elogic.it>
2022-11-22 18:13:33 +0100

    update index -partially

--- src/templates/static.tsx
+++ src/templates/static.tsx
@@ -2,7 +2,6 @@
  * This is an example of how to create a static template that uses getStaticProps to retrieve data.
  */
 import * as React from "react";
-import { fetch } from "@yext/pages/util";
 import "../index.css";
 import {
   Template,
@@ -14,26 +13,50 @@ import {
   TemplateProps,
   TemplateRenderProps,
 } from "@yext/pages";
-import PageLayout from "../components/page-layout";
-import Card from "../components/card";
-import { ExternalImage } from "../types/ExternalImage";
-import Favicon from "../public/yext-favicon.ico";
+import Footer from "../components/footer";
+
+export const config: TemplateConfig = {
+    stream: {
+        $id: "ce_root",
+        filter: {
+            savedFilterIds: ["dm_health-v2"]
+        },
+        fields: [
+            "id",
+            "name",
+            "slug",
+            "dm_directoryChildren.name",
+            "dm_directoryChildren.slug",
+            "dm_directoryChildren.dm_directoryChildren.name",
+            "dm_directoryChildren.dm_directoryChildren.c_nomeStruttura",
+            "dm_directoryChildren.dm_directoryChildren.id",
+            "dm_directoryChildren.dm_directoryChildren.slug",
+            "dm_directoryChildren.dm_directoryChildren.address",
+            "dm_directoryChildren.dm_directoryChildren.geocodedCoordinate",
+            "dm_directoryChildren.dm_directoryChildren.hours"
+        ],
+        localization: {
+            locales: ["it"],
+            primary: false,
+        },
+    },
+};
 
 /**
  * Not required depending on your use case.
  */
-export const config: TemplateConfig = {
+/*export const config: TemplateConfig = {
   // The name of the feature. If not set the name of this file will be used (without extension).
   // Use this when you need to override the feature name.
   name: "turtlehead-tacos",
-};
+};*/
 
 /**
  * A local type for transformProps. This could live in src/types but it's generally
  * best practice to keep unshared types local to their usage.
  */
-type ExternalImageData = TemplateProps & { externalImage: ExternalImage };
-
+/*type ExternalImageData = TemplateProps & { externalImage: ExternalImage };
+*/
 /**
  * Used to either alter or augment the props passed into the template at render time.
  * This function will be run during generation and pass in directly as props to the default
@@ -44,7 +67,7 @@ type ExternalImageData = TemplateProps & { externalImage: ExternalImage };
  *
  * If the page is truly static this function is not necessary.
  */
-export const transformProps: TransformProps<ExternalImageData> = async (
+/*export const transformProps: TransformProps<ExternalImageData> = async (
   data
 ) => {
   const url = import.meta.env.YEXT_PUBLIC_EXTERNAL_IMAGE_API_BASE_URL + "/2";
@@ -52,7 +75,7 @@ export const transformProps: TransformProps<ExternalImageData> = async (
     res.json()
   )) as ExternalImage;
   return { ...data, externalImage };
-};
+};*/
 
 /**
  * Defines the path that the generated file will live at for production.
@@ -60,14 +83,14 @@ export const transformProps: TransformProps<ExternalImageData> = async (
  * NOTE: This currently has no impact on the local dev path. Local dev urls currently
  * take on the form: featureName/entityId
  */
-export const getPath: GetPath<ExternalImageData> = () => {
-  return `index.html`;
+export const getPath: GetPath<TemplateProps> = ({ document }) => {
+    return `index.html`;
 };
-
+/*
 type ExternalImageRenderData = TemplateRenderProps & {
   externalImage: ExternalImage;
 };
-
+*/
 
 /**
  * This allows the user to define a function which will take in their template
@@ -91,14 +114,6 @@ type ExternalImageRenderData = TemplateRenderProps & {
           name: "description",
           content: "Static page example meta description.",
         },
-      },
-      {
-        type: "link",
-        attributes: {
-          rel: 'icon',
-          type: 'image/x-icon',
-          href: Favicon
-        },
       }
     ],
   };
@@ -109,55 +124,68 @@ type ExternalImageRenderData = TemplateRenderProps & {
  * This is the main template. It can have any name as long as it's the default export.
  * The props passed in here are the direct result from `getStaticProps`.
  */
-const Static: Template<ExternalImageRenderData> = ({
+const Static: Template<TemplateRenderProps> = ({
   relativePrefixToRoot,
   path,
-  document,
-  externalImage,
+  document
 }) => {
-  const { _site } = document;
+    const {
+        dm_directoryChildren
+    } = document;
 
-  return (
-    <>
-      <PageLayout _site={_site}>
-        <div className="centered-container">
-          <div className="bg-red-900 text-5xl font-bold text-white p-10 flex items-center justify-center flex-col gap-x-14 gap-y-10 md:flex-row">
-            <h1>Welcome to Turtlehead Tacos</h1>
-          </div>
-          <div className="space-y-5">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-              velit, sed quia non numquam eius modi tempora incidunt ut labore
-              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
-              veniam, quis nostrum exercitationem ullam corporis suscipit
-              laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
-              vel eum iure reprehenderit qui in ea voluptate velit esse quam
-              nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-              voluptas nulla pariatur?
-            </p>
-            <div>
-              <Card {...externalImage} />
+    const regioni: any = [];
+    const strutture: any = [];
+
+    const sortedRegioni = dm_directoryChildren.map((regione: any) => {
+        regioni.push(regione);
+        regioni.sort(function (a: any, b: any) {
+            var a = a.name, b = b.name;
+            return (a < b) ? -1 : (a > b) ? 1 : 0;
+        })
+    });
+
+    const sortedStrutture = dm_directoryChildren.map((entity: any) => {
+        { entity.dm_directoryChildren.map((struttura: any) => strutture.push(struttura)) }
+        strutture.sort(function (a: any, b: any) {
+            var a = a.c_nomeStruttura, b = b.c_nomeStruttura;
+            return (a < b) ? -1 : (a > b) ? 1 : 0;
+        })
+    });
+
+    return (
+        <>
+            <div className="flex min-h-screen w-full">
+                <div className="w-full">
+                    <div className="header sticky top-0">
+                       
+                    </div>
+                    <div className="w-full">
+                        {sortedStrutture}
+                    </div>
+                    <div className="section" data-ya-scope="SectionListRegione">
+                        <div className="container">
+                            <div className="lista_regioni">
+                                <h5 className="title_nearby">Ospedali GVM per Regione</h5>
+                                <ul className="regioni-list">
+                                    {regioni.map(r => {
+                                        return (
+                                            <li data-ya-track={"CTAListRegione_" + r.name}><a href={r.slug} title={"Ospedali GVM in " + r.name}>{r.name}</a></li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full bg-gray-100">
+                        <div className="">
+                            <Footer />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </PageLayout>
-    </>
-  );
+
+        </>
+    );
 };
 
 export default Static;
